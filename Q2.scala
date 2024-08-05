130637a614ec5507b792f6dd785b0008ecfb1a14
sadeepdeshal <140165441+sadeepdeshal@users.noreply.github.com>
2024-08-06 00:17:24 +0530

    Add files via upload

---
+++ Q2.scala
@@ -0,0 +1,17 @@
+package FP_Prac08
+
+object Q2{
+
+  def calculateSquare(lis: List[Int]): List[Int] = {
+    lis.map(x => x * x)
+  }
+  def main(args: Array[String]): Unit = {
+
+    val numbers=List(1, 2, 3, 4, 5)
+    val answer=calculateSquare(numbers)
+    println(answer.mkString("[", ", ", "]"))
+
+
+  }
+
+}
---
+++ Q3.scala
@@ -0,0 +1,19 @@
+package FP_Prac08
+
+object Q3 {
+
+  def main(args: Array[String]): Unit = {
+    val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    val isPrime = (num: Int) => {
+      if (num <= 1) false
+      else if (num == 2) true
+      else if (num % 2 == 0) false
+      else !(3 to Math.sqrt(num).toInt by 2).exists(num % _ == 0)
+    }
+
+    val primeNumbers = numbers.filter(isPrime)
+    
+    println(s"Prime numbers: ${primeNumbers.mkString(", ")}")
+  }
+}
