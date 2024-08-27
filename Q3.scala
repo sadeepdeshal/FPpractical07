bd74fa0a0a35649d07875906940f5deef38e01a2
sadeepdeshal <140165441+sadeepdeshal@users.noreply.github.com>
2024-08-27 08:55:22 +0530

    Update Q3.scala

--- Q3.scala
+++ Q3.scala
@@ -1,18 +1,13 @@
-object scala07 {
+object Q3 {
   def filterPrime(numbers: List[Int]): List[Int] = {
-    def isPrime(n: Int): Boolean = {
-      if (n <= 1) false
-      else if (n == 2) true
-      else !(2 until n).exists(i => n % i == 0)
-    }
-
-    numbers.filter(isPrime)
+    numbers.filter(n => 
+      n > 1 && !(2 until n).exists(i => n % i == 0)
+    )
   }
 
   def main(args: Array[String]): Unit = {
     val inputList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val primeNumbers = filterPrime(inputList)
-    println(primeNumbers.mkString(",")) 
+    println(primeNumbers.mkString(","))
   }
-
 }
